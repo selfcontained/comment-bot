2d671127da697988ec81a3afee079aa8b8977b8d
Mary Xia <maryx@syapse.com>
2016-07-28 13:15:39 -0700

    comma

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
         port: process.env.PORT,
         spreadsheet: {
             ttl: '15 minutes',
-            id: '1gn9Cpk7H8uK5ADhlUSHvNqdzwBLsLIYe0JY7IjOomyg'
+            id: '1gn9Cpk7H8uK5ADhlUSHvNqdzwBLsLIYe0JY7IjOomyg',
             sheet: 1
         },
         slack: {
